2369e0250485552062acfe43d8531fff49064e20
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-01-08 16:06:53 +0000

    added Ship itinteraries that have ports

--- __tests__/Ship.tests.js
+++ __tests__/Ship.tests.js
@@ -1,31 +1,53 @@
 /* globals describe it expect */
 const Ship = require('../src/Ship.js');
 const Port = require('../src/Port.js');
+const Itinerary = require('../src/Itinerary');
 
 describe('Ship', () => {
   it('can be instantiated', () => {
-    expect(new Ship()).toBeInstanceOf(Object);
+    const port = new Port('Liverpool');
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
+
+    expect(ship).toBeInstanceOf(Object);
   });
   it('has a starting port', () => {
     const port = new Port('Dover');
-    const ship = new Ship(port);
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
 
     expect(ship.currentPort).toBe(port);
   });
   it('can set sail', () => {
-    const port = new Port('Dover');
-    const ship = new Ship(port);
+    const dover = new Port('Dover');
+    const newYork = new Port('New York');
+    const itinerary = new Itinerary([dover, newYork]);
+    const ship = new Ship(itinerary);
+
     ship.setSail();
 
     expect(ship.currentPort).toBeFalsy();
-  })
+  });
   it('can dock at different ports', () => {
     const liverpool = new Port('Liverpool');
-    const ship = new Ship(liverpool);
-
     const newYork = new Port('New York');
-    ship.dock(newYork);
+    const itinerary = new Itinerary([liverpool, newYork]);
+    const ship = new Ship(itinerary);
+
+    ship.setSail();
+    ship.dock();
 
     expect(ship.currentPort).toBe(newYork);
-  })
+  });
+  it('cannot sail further than its itinerary', () => {
+    const liverpool = new Port('Liverpool');
+    const newYork = new Port('New York');
+    const itinerary = new Itinerary([liverpool, newYork]);
+    const ship = new Ship(itinerary);
+
+    ship.setSail();
+    ship.dock();
+
+    expect(() => ship.setSail()).toThrowError('End of itinterary reached');
+  });
 });
